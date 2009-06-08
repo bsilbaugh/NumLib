6c66ce0a0113f066fdec51d925697ebb7bac0668
Benjamin Silbaugh <silbaugh@pandemonium.umd.edu>
2009-06-08 14:15:09 -0400

    Simplified SparsePattern - now just a 1D Index array

--- src/linalg/SparsePattern.h
+++ src/linalg/SparsePattern.h
@@ -4,55 +4,11 @@
 #ifndef SPARSEPATTERN_H
 #define SPARSEPATTERN_H
 
-#include "numlib-config.h"
-#include "debug_tools.h"
+#include "../array/Array1D.h"
 
 namespace numlib{ namespace linalg{
 
-template< class KeyType >
-class SparsePattern
-{
-public:
-
-  //! Create a sparsity pattern for "nn" non-zero elements
-  SparsePattern(Size nn_=0);
-
-  //! Copy constructor (deep copy)
-  SparsePattern(const SparsePattern & other);
-
-  //! Destructor
-  ~SparsePattern();
-
-  //! Assignment (deep copy)
-  SparsePattern & operator=(const SparsePattern & other);
-
-  //! Returns the number of non-zero elements
-  Size size();
-
-  //! Sets/returns non-zero element key value by index
-  /*! 
-   *  Index range is [0,nn-1]. All key values are assumed to be sorted
-   *  and "increase" with index value.
-   */
-  KeyType & operator[](Index k);
-
-  //! Returns non-zero element key value by index
-  const KeyType & operator[](Index k) const;
-
-private:
-
-  // Number of non-zero elements
-  Size nn;
-
-  // Array of non-zero element keys
-  KeyType* keys;
-
-  // copies data from "other" to "this"
-  void copy(const SparsePattern & other);
-
-};
-
-#include "SparsePattern-inl.h"
+typedef Array1D<Index> SparsePattern;
 
 }}//::numlib::linalg
 
