4baee52aa1df0a6d92fea68f65142b776a0824b2
Benjamin Silbaugh <silbaugh@pandemonium.umd.edu>
2009-10-23 17:06:56 -0400

    Added reconstruction operator for rotation fields

---
+++ src/reconstruction/RotInterp-inl.h
@@ -0,0 +1,70 @@
+/*! \file RotInterp-inl.h
+ */
+
+#include "RotInterp-inl.h"
+
+namespace numlib{ namespace reconstruction{
+
+RotInterp<GridType,TensorFieldType>::RotInterp(const GridType& srcGrid, 
+											   const GridType& dstGrid):
+	nsrc(srcGrid.size()),
+	ndst(dstGrid.size()),
+	interp(srcGrid,dstGrid)
+{
+}
+
+RotInterp<GridType,TensorFieldType>::RotInterp(const RotInterp& other):
+	nsrc(other.nsrc),
+	ndst(other.ndst),
+	interp(other.interp)
+{
+}
+
+RotInterp<GridType,TensorFieldType>::~RotInterp()
+{
+	/* nothing to delete */
+}
+
+RotInterp& 
+RotInterp<GridType,TensorFieldType>::operator=(const RotInterp& other)
+{
+	if(&other == this) return *this;
+
+	nsrc = other.nsrc;
+	ndst = other.ndst;
+	interp = other.interp;
+
+	return *this;
+}
+
+void 
+RotInterp<GridType,TensorFieldType>::operator()(const TensorFieldType& srcField,
+												const TensorFieldType& dstField)
+{
+	ASSERT(srcField.size() == nsrc);
+	ASSERT(dstField.size() == ndst);
+
+	// Set reference rotation...
+
+	TensorR2 r0 = srcField(0);
+
+	// Compute rotation increments...
+
+	RotVecField rvsrc(nsrc);
+	RotVecField rvdst(ndst);
+
+	for(Index i=0; i<nsrc; ++i)
+		tensor::rotInc(srcField(i), r0, rvsrc(i));
+
+	// Interpolate rotation increments...
+
+	interp(rvsrc, rvdst);
+
+	// Compute rotation field...
+
+	for(Index i=0; i<ndst; ++i)
+		dstField(i) = tensor::dot(r0, rot(rvdst(i)));
+
+}
+
+}}//::numlib::reconstruction
---
+++ src/reconstruction/RotInterp.h
@@ -0,0 +1,51 @@
+/*! \File RotInterp.h
+ */
+
+#ifndef ROTINTERP_H
+#define ROTINTERP_H
+
+#include "../tensor/RotVector.h"
+#include "../tensor/FiniteRotations.h"
+#include "LinearPW.h"
+
+namespace numlib{ namespace reconstruction{
+
+//! Interpolates a rotation field using LinearPW
+/*!
+ *  Currently assumes that the total rotation increment over the
+ *  entire field is in [0,pi]. Future implementations may rectify
+ *  this simplifying assumption.
+ */
+template<class GridType, class TensorFieldType>
+class RotInterp
+{
+public:
+
+	RotInterp(const GridType& srcGrid, const GridType& dstGrid);
+
+	RotInterp(const RotInterp& other);
+
+	~RotInterp();
+
+	RotInterp& operator=(const RotInterp& other);
+
+	void operator()(const TensorFieldType& srcField, TensorFieldType& dstField);
+
+private:
+
+	typedef numlib::array::Array1D<tensor::RotVector> RotVecField;
+
+	//! Size of source rotation field
+	Size nsrc;
+
+	//! Size of destination rotation field
+	Size ndst;
+
+	//! Interpolation operator for rotation increments
+	LinearPW<GridType, RotVecField> interp;
+
+};
+
+}}//::numlib::reconstruction
+
+#endif
