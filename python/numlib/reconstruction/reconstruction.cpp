14bf7e3493c17ff26daad37d1e5e7b3eb1d0e6fc
Benjamin Silbaugh <silbaugh@pandemonium.umd.edu>
2009-10-23 17:29:02 -0400

    Extended python reconstruction module to include common field types
    The implementation of this module is still "experimental"
    
    Design of python reconstruction module addresses short term needs

--- python/numlib/reconstruction/reconstruction.cpp
+++ python/numlib/reconstruction/reconstruction.cpp
@@ -5,19 +5,27 @@
 
 #include <boost/python.hpp>
 #include "numlib/array/Array1D.h"
+#include "numlib/tensor/TensorR1.h"
+#include "numlib/tensor/TensorR2.h"
 #include "numlib/reconstruction/LinearPW.h"
+#include "numlib/reconstruction/RotInterp.h"
 
 using namespace numlib;
+using numlib::tensor::TensorR1;
+using numlib::tensor::TensorR2;
 
-typedef numlib::array::Array1D<Real> Grid;
-typedef numlib::array::Array1D<Real> Field;
-typedef numlib::reconstruction::LinearPW<Grid, Field> LinearPW;
+typedef numlib::array::Array1D<Real> Grid1D;
+typedef numlib::array::Array1D<Real> ScalarField1D;
+typedef numlib::array::Array1D<TensorR1> VectorField1D;
+typedef numlib::array::Array1D<TensorR2> TensorField1D;
+typedef numlib::reconstruction::LinearPW<Grid1D, ScalarField1D> ScalarInterp;
+typedef numlib::reconstruction::LinearPW<Grid1D, VectorField1D> VectorInterp;
+typedef numlib::reconstruction::RotInterp<Grid1D, TensorField1D> RotationInterp;
 
-Field interpolate(LinearPW& self, const Field& src)
+template<class I, class F>
+void interpolate(I& self, const F& src, F& dst)
 {
-	Field dst(src.size());
 	self(src, dst);
-	return dst;
 }
 
 BOOST_PYTHON_MODULE( _reconstruction )
@@ -25,7 +33,16 @@ BOOST_PYTHON_MODULE( _reconstruction )
 
 	using namespace boost::python;
 
-	class_< LinearPW >("LinearPW", init< const Grid&, const Grid& >())
-		.def("__call__", &interpolate)
+	class_< ScalarInterp >("ScalarInterp", init< const Grid1D&, const Grid1D& >())
+		.def("__call__", &interpolate<ScalarInterp, ScalarField1D>)
 	;
+
+	class_< VectorInterp >("VectorInterp", init< const Grid1D&, const Grid1D& >())
+		.def("__call__", &interpolate<VectorInterp, VectorField1D>)
+	;
+
+	class_< RotationInterp >("RotationInterp", init< const Grid1D&, const Grid1D& >())
+		.def("__call__", &interpolate<RotationInterp, TensorField1D>)
+	;
+
 }
