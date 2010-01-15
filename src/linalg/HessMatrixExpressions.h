a56b1c2c8ff32e71573fba334fde36f82699c9f2
Benjamin Silbaugh <silbaugh@pandemonium.umd.edu>
2010-01-14 19:56:51 -0500

    Added experimental LU solver for HessMatrix which uses pivoting

--- src/linalg/HessMatrixExpressions.h
+++ src/linalg/HessMatrixExpressions.h
@@ -34,9 +34,12 @@ Vector<T> prod(const HessMatrix<T> & a, const Vector<T> & u)
 	 return v;
 }
 
-//! Computes the LU factorization of [A]
+//! Computes the ``Doolittle'' LU factorization of [A]
 /*!
  *  LU factorization is performed in place to minimize memory requirements
+ *
+ *  In ``Doolittle'' factorization, the 1's are on the main diagonal of
+ *  the lower diagonal matrix.
  */
 template<class T>
 void factorLU(HessMatrix<T> & hess)
@@ -73,6 +76,8 @@ void factorLU(HessMatrix<T> & hess)
  *  Upon input, 'hessLU' is the LU factorization of an (upper) Hessenberg 
  *  coefficient matrix [A], and 'x' is the RHS vector {b}. Upon output, 
  *  'x' is overwritten with the solution vector {x}.
+ *
+ *  ``Doolittle'' LU factorization is assumed (1's on the diagonal of [L])
  */
 template<class T>
 void solveLU(const HessMatrix<T> & hessLU, Vector<T> & x)
@@ -102,6 +107,113 @@ void solveLU(const HessMatrix<T> & hessLU, Vector<T> & x)
 		  z = 0;
 		  for(Index j = i; j < n; ++j) z += hessLU(im1,j)*x(j);
 		  x(im1) = (x(im1) - z)/hessLU(im1,im1);
+	 }
+}
+
+//! Computes the LU factorization of [A] with row pivoting
+/*!
+ *  LU factorization is performed in place to minimize memory requirements.
+ *  Row pivoting is used to improve numerical conditioning.
+ *
+ */
+template<class T>
+void factorLU(HessMatrix<T> & hess, Vector<Index> & p)
+{
+	 // Get problem size...
+
+	 ASSERT( hess.size2() == hess.size1() );
+
+	 Size n = hess.size2();
+
+	 ASSERT( p.size() == n );
+
+	 // Initialize pivot vector...
+
+	 for(Index i=0; i<n; ++i)
+		 p(i) = i;
+
+	 // Compute LU factorization...
+
+	 T c;
+	 for(Index i=1; i<n; ++i)
+	 {
+
+		 // Pivot rows...
+
+		 if( fabs(hess(p(i-1),i-1)) < fabs(hess(p(i),i-1)) )
+		 {
+			 Index im1 = p(i-1);
+			 p(i-1) = p(i);
+			 p(i) = im1;
+		 }
+
+		 // Compute elements of [L] for ith row...
+
+		 /* REMINDER: L_{i,i} = 1.0 and L_{i,j} = 0 for all 0 < j < i-1 */
+
+		 c = hess(p(i),i-1)/hess(p(i-1),i-1);
+		 hess(p(i),i-1) = c; // L_{i,i-1}
+
+		 // Compute elements of [U] for ith row...
+
+		 for(Index j=i; j<n; ++j) hess(p(i),j) -= c*hess(p(i-1),j);
+
+	 }
+
+}
+
+//! Directly solves [A]{x} = {b} where [A] = [L][U] is (upper) Hessenberg
+/*!
+ *  Upon input, 'hessLU' is the LU factorization of an (upper) Hessenberg 
+ *  coefficient matrix [A], and 'x' is the RHS vector {b}. Upon output, 
+ *  'x' is overwritten with the solution vector {x}.
+ *
+ *  This version employs row pivoting defined by the permutation vector 'p'.
+ *
+ *  *** THIS IS EXPERIMENTAL - CAVEAT EMPTOR ****
+ *
+ *  \todo Work out sparisity issues with [L]; when pivoting is used the
+ *  structure of [L] is no longer banded.
+ */
+template<class T>
+void solveLU(const HessMatrix<T> & hessLU, const Vector<Index> & p, Vector<T> & x)
+{
+
+	 /**** Still under development -- don't use ****/
+	 /*  Need to work out sparsity of [L] when pivoting is used */
+	 ASSERT( false );
+
+	 // Get problem size...
+
+	 ASSERT( hessLU.size1() == hessLU.size2() );
+
+	 Size n = hessLU.size2();
+
+	 ASSERT( x.size() == n );
+	 ASSERT( p.size() == n );
+
+	 // Initialize intermediate vector {d} ...
+
+	 Vector<T> d(n);
+
+	 // Solve [L]{d} = {b} where {b} rhs vector contained in 'x' ...
+
+	 DEBUG_PRINT( "Solving lower system..." );
+	 d(0) = x(p(0));
+	 for(Index i = 1; i<n; ++i)
+		  d(i) = x(p(i)) - hessLU(p(i),i-1)*d(i-1);
+
+	 // Solve [U]{x} = {d} ...
+	 DEBUG_PRINT( "Solving upper system..." );
+	 T z;
+	 Index im1;
+	 x(n-1) = d(n-1)/hessLU(p(n-1),n-1);
+	 for(Index i=n-1; i > 0; --i)
+	 {
+		  im1 = i-1;
+		  z = 0;
+		  for(Index j = i; j < n; ++j) z += hessLU(p(im1),j)*x(j);
+		  x(im1) = (d(im1) - z)/hessLU(p(im1),im1);
 	 }
 }
 
