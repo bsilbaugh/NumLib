ba237daf130ce560f1156c3bc4fa02369a8fde05
Benjamin Silbaugh <silbaugh@pandemonium.(none)>
2010-04-12 10:56:11 -0400

    Added recursive factorial function

---
+++ src/base/factorial.h
@@ -0,0 +1,22 @@
+/*! \file factorial.h
+ */
+
+#ifndef FACTORIAL_H
+#define FACTORIAL_H
+
+#include "numlib-config.h"
+
+namespace numlib{
+
+//! Recursive factorial function for integer type Int
+inline
+T factorial(const Int& k)
+{
+	if(x==0)
+		return k;
+	return k*factorial(k-1);
+}
+
+}//::numlib
+
+#endif
