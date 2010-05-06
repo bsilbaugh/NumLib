448c6b6be561ddcb9699c1942547075e9f467c82
Benjamin Silbaugh <silbaugh@pandemonium.(none)>
2010-05-06 16:34:37 -0400

    Redesigned OptimizerCG
    OptimizerCG was redesigned to allow for easier solution monitoring,
    ``tighter'' coupling with other processes, and implementation of
    ad-hoc iteration termination rules. The new interface is now similar
    to that of NewtonKrylov nonlinear solver.

--- src/optimization/OptimizerCG.h
+++ src/optimization/OptimizerCG.h
@@ -20,85 +20,144 @@ public:
 
 	typedef typename CostFunction::VectorType VectorType;
 
-	OptimizerCG(Real bf_step=1.0, Real gs_reduction=0.1, Real tol=5.0E-3, Size max_iter_=1000):
-		max_iter(max_iter_), delta_f_max(tol), line_search(bf_step, gs_reduction, tol)
+	typedef MinimumND<VectorType> Minimum;
+
+	OptimizerCG(Size dim, Real bf_step=1.0, Real gs_reduction=0.1, Real tol_=5.0E-3, Size max_iter_=1000):
+		n(dim), tol(tol_), max_iter(max_iter_), dir(n), grad(n),
+		line_search(bf_step, gs_reduction, tol)
 	{}
 
-	ErrorCode minimize(CostFunction& f, MinimumND<VectorType>& min)
+	//! Sets convergence tolerance used to terminate execution of OptimizerCG::minimize
+	void tolerance(Real tol_) {tol = tol_;}
+
+	//! Returns tolerance used to terminate execution of OptimizerCG::minimize
+	const Real tolerance() const {return tol;}
+
+	//! Sets maximum allowable iterations executed by OptimizerCG::minimize
+	void maxIteration(Size max_iter_) {max_iter = max_iter_;}
+
+	//! Sets maximum allowable iterations executed by OptimizerCG::minimize
+	const Size maxIteration() const {return max_iter;}
+
+	//! Resets optimizer to begin a new iteration sequence...
+	ErrorCode newSequence(CostFunction& f, const Minimum& min)
 	{
-
-		// Get initial design...
-
-		VectorType xopt(min.design());
-		Real fopt = min.cost();
-
-		// Set up temporary data structure...
-
-		VectorType grad_prev(f.size());
-		VectorType grad(f.size());
-		VectorType dir(f.size());
-		LineRes line_res(f, xopt, dir); /* this holds ref to xopt and dir! */
-		Real delta_f;
-
 		// Compute initial search direction (steepest descent)...
 
 		f(min.design(), grad);
-		dir = grad;
+		dir  = grad;
 		dir *= -1.0;
 
-		// Perform sequential line searches to find minimum...
+		return SUCCESS;
+	}
+
+	//! Returns search direction to be used during next call to iter
+	/*!
+	 *  This is provided for monitoring purposes and can otherwise
+	 *  be ignored.
+	 */
+	const VectorType& searchDirection() const {return dir;}
+
+	//! Executes one iteration: refines the optimum design estimate
+	ErrorCode iter(CostFunction& f, Minimum& min)
+	{
+		VectorType xopt = min.design();
+		Real fopt = min.cost();
+
+		// Set up restriction of f to 1-dimensional subspace...
+
+		LineRes line_res(f, xopt, dir);
+
+		// Execute line search...
+
+		Minimum1D line_min(0.0, fopt);
+
+		ErrorCode code = line_search.minimize(line_res, line_min);
+
+		if(code!=SUCCESS) return code;
+
+		// Update design and cost...
+
+		xopt += line_min.design()*dir;
+		fopt  = line_min.cost();
+
+		min.design(xopt, fopt);
+
+		// Update search direction using Polak-Ribiere formula...
+
+		VectorType grad_prev(grad);
+
+		f(xopt, grad);
+
+		Real beta = prod(grad, grad - grad_prev)/prod(grad, grad);
+
+		dir *= beta;
+		dir += grad;
+
+		return SUCCESS;
+
+	}
+
+	//! Executes a complete optimization sequence until convergence or falure
+	/*!
+	 *  This is essentially a wrapper around OptimizerCG::newSequence and
+	 *  OptimizerCG::iter. If neither solution monitoring, intermediate 
+	 *  processing, nor special termination rules for the iteration sequence 
+	 *  are needed, then this function may be called in place of 
+	 *  OptimizerCG::newSequence and OptimizerCG::iter. However, this function 
+	 *  does not need to be called if the user is explicitly calling 
+	 *  OptimizerCG::newSequence and OptimizerCG::iter from within their own 
+	 *  "minimize" function.
+	 *
+	 *  For those needing a tighter integration of OptimizerCG with another 
+	 *  component, the implementation of this function may also be used as an 
+	 *  example of how OptimizerCG::newSequence and OptimizerCG::iter are used 
+	 *  to compute an optimal solution.
+	 */
+	ErrorCode minimize(CostFunction& f, Minimum& min)
+	{
+		Real fopt = min.cost();
+		ErrorCode code = SUCCESS;
+
+		// Initialize optimization search...
+
+		code = newSequence(f, min);
+
+		if(code!=SUCCESS) return code;
+
+		// Sequentially perform line searches...
 
 		for(Index n=0; n<max_iter; ++n)
 		{
-			// Perform line search...
-
-			Minimum1D line_min(0.0, fopt);
-
-			ErrorCode code = line_search.minimize(line_res, line_min);
-
+			// Execute line search and check for error...
+		
+			code = iter(f, min);
 			if(code!=SUCCESS) return code;
 
-			// Compute convergence criteria...
+			// Check convergence criteria...
 
-			delta_f = fopt - line_min.cost();
-
-			// Update design and cost...
-
-			xopt += line_min.design()*dir;
-			fopt -= delta_f;
-
-			// Check convergence and exit if satisfied...
-
-			if( fabs(delta_f) < delta_f_max )
-			{
-				min.design(xopt, fopt);
-				return SUCCESS;
-			}
-		
-			// Update search direction...
-
-			grad_prev = grad;
-
-			f(xopt, grad);
-
-			Real beta = prod(grad, grad - grad_prev)/prod(grad, grad);
-
-			dir *= beta;
-			dir += grad;
-
+			Real delta_f = fopt - min.cost();
+			if(fabs(delta_f) < tol) return code;
 		}
 
-		return EXCEEDED_MAX_ITER;
-
+		return ERROR;
 	}
 
 private:
 
+	DISALLOW_COPY_AND_ASSIGN( OptimizerCG );
+
 	typedef LineRestriction<CostFunction> LineRes;
+
+	Size n;
+
+	Real tol;
 
 	Size max_iter;
 
-	Real delta_f_max;
+	VectorType dir;
+
+	VectorType grad;
 
 	OptimizerBGQ< LineRes > line_search;
 
