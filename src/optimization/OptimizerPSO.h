dc4ce09afd446ec8ff633d11a37b11ec739e5d18
Benjamin Silbaugh <silbaugh@pandemonium.(none)>
2010-05-13 22:02:54 -0400

    Added Particle Swarm Optimization to optimization library

---
+++ src/optimization/OptimizerPSO.h
@@ -0,0 +1,184 @@
+/*! \file OptimizerPSO.h
+ *  \brief OptimizerPSO class template definition
+ */
+
+#ifndef OPTIMIZER_PSO_H
+#define OPTIMIZER_PSO_H
+
+#include "../array/PointerArray.h"
+
+#include "Particle.h"
+#include "MinimumND.h"
+
+namespace numlib{ namespace optimization{
+
+//! Multi-variate optimizer based on Particle Swarm Optimization (PSO)
+/*!
+ *	This optimizer implements the Particle Swarm Optimization (PSO)
+ *	algorithm introduced by Eberhart and Kennedy. PSO is a zeroth
+ *	order heuristic optimization algorithm. Like Genetic Algorithms (GA),
+ *	and Simulated Annealing (SA), PSO is fundamentally a statistical 
+ *	algorithm which, given the right choice of parameters, gives a
+ *	strong likelyhood of finding a global minimum. PSO may be applied
+ *	to multimodal and mixed continous-discrete optimization problems.
+ *	The current implementation assumes continuous variables; however,
+ *	it has been shown that rounding to the nearest integer gives
+ *	reasonable results.
+ *
+ *	It is assumed that the design space is nominally contained in the
+ *	unit d-cube. Particles will be allowed to leave this area, however,
+ *	all initial partical positions will fall within these limits. The
+ *	recommendation is that your cost function be scaled such that
+ *	these assumptions hold.
+ *
+ *	In regards to a stopping criterion, the usual convention is to
+ *	monitor the change in the estimated minimum cost and stop when
+ *	this values falls below a user specified threshold. For this
+ *	kind of algorithm, this criterion does can result in premature
+ *	termination. A plot of the cost estimate versus iteration will
+ *	show a monotone non-increasing graph with many plateaus. Unfortunately,
+ *	a rigourous stopping criterion does not currently exist for PSO.
+ *
+ *	\todo Find better container type for swarm - one with a proper iterator
+ *	would be nice.
+ *
+ *	\todo Replace use of Minimum with a generic Swarm? This would give
+ *	the user more flexibility in setting initial conditions and monitoring
+ *	solution progress.
+ */
+template<class F, class V, class P=Particle<F, V> >
+class OptimizerPSO
+{
+public:
+
+	typedef F FunctionType;
+	typedef V VectorType;
+	typedef P ParticleType;
+	typedef MinimumND<VectorType> Minimum;
+
+	OptimizerPSO(Size dim, Size np, Real tol_=5.0E-3, Size max_iter_=1000):
+		swarm(np),gbest(NULL), tol(tol_), max_iter(max_iter_)
+	{
+		for(Index i=0; i<np; ++i)
+			swarm[i] = new ParticleType(dim, i);
+		gbest = swarm[0]; /* default value */
+	}
+
+	~OptimizerPSO()
+	{
+		swarm.clear();
+	}
+
+	//! Returns the number of particles
+	Size size() const {return swarm.size();}
+
+	//! Returns const pointer to the ith particle
+	const ParticleType* particle(Index i) const {return swarm[i];}
+
+	//! Initializes particle positions and velocities in the design space
+	ErrorCode newSequence(FunctionType& f, Minimum& min)
+	{
+		// Initialize particles...
+
+		for(Index i=0; i<swarm.size(); ++i)
+			swarm[i]->newSequence(f);
+
+		// Find best...
+
+		gbest = swarm[0];
+		for(Index i=1; i<swarm.size(); ++i)
+		{
+			if(gbest->bestCost() > swarm[i]->bestCost())
+				gbest = swarm[i];
+		}
+
+		// Set best...
+
+		for(Index i=0; i<swarm.size(); ++i)
+			swarm[i]->bestParticle(gbest);
+
+		min.design( gbest->bestPosition(), gbest->bestCost() );
+
+		return SUCCESS;
+	}
+
+	//! Advances the dynamics of the swarm one time level
+	ErrorCode iter(FunctionType& f, Minimum& min)
+	{
+		// Advance swarm...
+
+		for(Index i=0; i<swarm.size(); ++i)
+			swarm[i]->iter(f);
+
+		// Find best...
+
+		gbest = swarm[0];
+		for(Index i=1; i<swarm.size(); ++i)
+		{
+			if(gbest->bestCost() > swarm[i]->bestCost())
+				gbest = swarm[i];
+		}
+
+		// Set best...
+
+		for(Index i=0; i<swarm.size(); ++i)
+			swarm[i]->bestParticle(gbest);
+
+		// Update solution..
+
+		min.design( gbest->bestPosition(), gbest->bestCost() );
+
+		return SUCCESS;
+
+	}
+
+	ErrorCode minimize(FunctionType& f, Minimum& min)
+	{
+
+
+		ErrorCode code = newSequence(f, min);
+
+		for(Index n=0; n<max_iter; ++n)
+		{
+			Real fopt = min.cost();
+
+			code = iter(f, min);
+
+			if(code!=SUCCESS) return code;
+
+			Real delta_f = fopt - min.cost();
+
+			ASSERT( !(delta_f < 0) );
+
+			if(delta_f < tol) /* this is not the best criterion */
+			{
+				min.cost( gbest->bestCost() );
+				min.design( gbest->bestPosition() );
+				return SUCCESS;
+			}
+
+		}
+
+		return EXCEEDED_MAX_ITER;
+
+	}
+
+private:
+
+	DISALLOW_COPY_AND_ASSIGN( OptimizerPSO );
+
+	typedef array::PointerArray<ParticleType> Swarm;
+
+	Swarm swarm;
+
+	const ParticleType* gbest;
+
+	Real tol;
+
+	Size max_iter;
+
+};
+
+}}//::numlib::optimization
+
+#endif
---
+++ src/optimization/Particle.h
@@ -0,0 +1,171 @@
+/*! \file Particle.h
+ *  \brief Particle class template definition
+ */
+
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
+#include "../base/random.h"
+
+namespace numlib{ namespace optimization{
+
+//! Model of a basic ``particle'' used in Particle Swarm Optimization (PSO)
+/*!
+ *  The dynamics of this particle are based on the standard particle model
+ *  defined by Kenney and Eberhart. However, more sophisticated dynamic
+ *  models could be contrived. Alternative particle classes may be used
+ *  with OptimizerPSO provided that these alternative particle classes
+ *  implement the interface defined here.
+ *
+ */
+template<class F, class V>
+class Particle
+{
+public:
+
+	typedef F FunctionType;
+	typedef V VectorType;
+
+	Particle(Size dim, Index id_):
+		id(id_),
+		w(1.0), c1(1.5), c2(2.5),
+		cst(0), pos(dim), vel(dim),
+		pbest_cst(0), pbest_pos(dim),
+		gbest(NULL)
+	{}
+
+	Particle(const Particle& other):
+		w(other.w), c1(other.c1), c2(other.c2), 
+		cst(other.cst), pos(other.pos), vel(other.vel), 
+		pbest_cst(other.pbest_cst), pbest_pos(other.pbest_pos),
+		gbest(other.gbest)
+	{}
+
+	Particle operator=(const Particle& other)
+	{
+		if(this==&other) return *this;
+
+		w = other.w;
+		c1 = other.c1;
+		c2 = other.c2;
+		cst = other.cst;
+		pos = other.pos;
+		vel = other.vel;
+		pbest_cst = other.pbest_cst;
+		pbest_pos = other.pbest_pos;
+		gbest = other.gbest;
+
+		return *this;
+	}
+
+	~Particle()
+	{
+		/* all data auto delete */
+	}
+
+	void inertia(Real w_) {w = w_;}
+
+	void selfTrust(Real c1_) {c1 = c1_;}
+
+	void swarmTrust(Real c2_) {c2 = c2_;}
+
+	const Real inertia() const {return w;}
+
+	const Real confidence() const {return c1;}
+
+	const Real trust() const {return c2;}
+	
+	const VectorType& bestPosition() const {return pbest_pos;}
+
+	const Real bestCost() const {return pbest_cst;}
+
+	const VectorType& position() const {return pos;}
+
+	const VectorType& velocity() const {return vel;}
+
+	const Real& cost() const {return cst;}
+
+	void newSequence(FunctionType& f)
+	{
+		// Set initial position and velocity...
+
+		ASSERT( vel.size() == pos.size() );
+
+		Size dim = pos.size();
+
+		randomSeed();
+
+		for(Index i=0; i<dim; ++i)
+		{
+			pos(i) = 2.0*randomNumber() - 1.0;
+			vel(i) = 2.0*randomNumber() - 1.0;
+		}
+
+		// Compute cost at initial position...
+
+		cst = f(pos);
+
+		// Set pbest to initial position and cost...
+
+		pbest_pos = pos;
+		pbest_cst = cst;
+
+		// Initialize gbest to self...
+
+		gbest = this;
+
+	}
+
+	void bestParticle(const Particle* gbest_) {gbest = gbest_;}
+
+	void iter(FunctionType& f)
+	{
+		// Update velocity...
+
+		Real r1 = randomNumber();
+		Real r2 = randomNumber();
+
+		vel *= w;
+		vel += c1*r1*(pbest_pos - pos);
+		vel += c2*r2*(gbest->bestPosition() - pos);
+
+		// Update position...
+		
+		pos += vel;
+
+		// Update cost...
+
+		cst = f(pos);
+
+		// Update personal best...
+
+		if(cst < pbest_cst)
+		{
+			pbest_cst = cst;
+			pbest_pos = pos;
+		}
+	}
+
+private:
+
+	Index id;
+
+	Real w, c1, c2;
+
+	Real cst;
+
+	VectorType pos;
+
+	VectorType vel;
+
+	Real pbest_cst;
+
+	VectorType pbest_pos;
+
+	const Particle* gbest;
+
+};
+
+}}//::numlib::optimization
+
+#endif
