c2ec50fe67377b0b3a1fc8341c4eafb519071aec
Benjamin Silbaugh <silbaugh@umd.edu>
2011-05-07 16:45:29 -0400

    Added fromString utility for extracting numerical data from strings
    This was copied from the Rodymol project. (I'm the author of both
    codes, so I can do this.)

--- src/base/io.h
+++ src/base/io.h
@@ -16,6 +16,20 @@ std::ostream & fmtReal(std::ostream & os)
 	 return os<<std::scientific<<std::setprecision(8)<<std::setw(15);
 }
 
+//! A utility for extracting numeric types from strings
+/*!
+ *  The third parameter should be one of the following:
+ *  - std::hex
+ *  - std::dec
+ *  - std::oct
+ */
+template<class T>
+bool fromString(T& t, const String & s, std::ios_base& (*f)(std::ios_base&)=std::dec)
+{
+	 std::istringstream iss(s);
+	 return !(iss >> f >> t).fail();
+}
+
 }
 
 #endif
