3da7c0e461d90af34964f4d630f894156b9987b8
Benjamin Silbaugh <silbaugh@umd.edu>
2011-06-24 14:26:12 -0400

    Added TableLoader

---
+++ src/io/TableLoader.cpp
@@ -0,0 +1,50 @@
+
+#include "TableLoader.h"
+
+namespace numlib{ namespace io{
+
+TableLoader::TableLoader(const String& file_name):
+	ncol(0), nrow(0), data(0)
+{
+	typedef std::vector<Real> Row;
+
+	// Load table...
+
+	std::ifstream ifile(file_name.c_str());
+	if(!ifile.is_open())
+		throw FileIOError( (file_name + " failed to open").c_str() );
+
+	while(true)
+	{
+		String line;
+		std::stringstream buff;
+
+		getline(ifile, line);
+		if(!ifile) break;
+		if(line.size() == 0) continue;
+		buff<<line;
+
+		Real val;
+		Row row(0);
+		while(buff>>val)
+			row.push_back(val);
+
+		data.push_back(row);
+	}
+
+		ifile.close();
+
+		// Get table size...
+	
+		nrow = data.size() - 1;
+		ncol = data[0].size();
+
+		// Check that all row are the same size...
+
+		for(Index j=1; j<nrow; ++j)
+			if(!(data[j].size() == ncol + 1))
+				throw NumLibError("Table contains unequal columns");
+
+}
+
+}}//::namespace::io
---
+++ src/io/TableLoader.h
@@ -0,0 +1,49 @@
+
+#ifndef TABLE_LOADER_H
+#define TABLE_LOADER_H
+
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+#include "../base/numlib-config.h"
+#include "string_utils.h"
+#include "FileIOError.h"
+
+namespace numlib{ namespace io{
+
+class TableLoader
+{
+public:
+
+	//! Loads table from ASCII file
+	TableLoader(const String& file_name);
+
+	//! Returns the number of data rows in the table
+	const Size number_of_rows() const { return nrow; }
+
+	//! Returns the number of data columns in the table
+	const Size number_of_columns() const { return ncol; }
+
+	//! Return the grid associated with table rows
+	const Real abscissa(const Index i) const { return data[i+1][0]; }
+
+	//! Returns the grid associated with table columns
+	const Real ordinate(const Index j) const { return data[0][j]; }
+
+	//! Returns the table data value associated with ith row and jth column
+	const Real operator()(const Index i, const Index j) const { return data[i+1][j+1]; }
+
+private:
+
+	typedef std::vector< std::vector< Real > > Data;
+
+	Size ncol;
+	Size nrow;
+	Data data;
+
+};
+
+}}//::numlib::io
+
+#endif
