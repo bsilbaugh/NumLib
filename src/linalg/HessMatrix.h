a1cc6e5831beae790af68594e05b93250996e5dd
Benjamin Silbaugh <silbaugh@umd.edu>
2012-11-02 17:54:23 -0400

    Copied contents of HessMatrix-inl.h into HessMatrix.h
    I'm finding the use of *-inl.h files to be really obnoxious

--- src/linalg/HessMatrix.h
+++ src/linalg/HessMatrix.h
@@ -88,7 +88,131 @@ private:
 
 };
 
-#include "HessMatrix-inl.h"
+/*----------------------------------------------------------------------------*/
+/*                                                     IMPLEMENTATION DETAILS */
+
+template<class T>
+HessMatrix<T>::HessMatrix(Size m_):
+	 m(m_),
+	 columns(NULL),
+	 zero(0)
+{
+	columns = new Vector<T>[m];
+	for(Index j=0; j<m-1; ++j)
+		columns[j].resize(j+2);
+	columns[m-1].resize(m);
+}
+
+template<class T>
+HessMatrix<T>::HessMatrix(const HessMatrix & other):
+	 m(0),columns(0),zero(0)
+{
+	 m = other.m;
+	 columns = new Vector<T>[m];
+	 for(Index j=0; j<m; ++j)
+		 columns[j] = other.columns[j];
+}
+
+template<class T>
+HessMatrix<T>::HessMatrix(const ExtHessMatrix<T> & other):
+	 m(0),columns(NULL),zero(0)
+{
+	m = other.m;
+	columns = new Vector<T>[m];
+	for(Index j=0; j<m-1; ++j)
+		columns[j] = other.columns[j];
+	for(Index i=0; i<m; ++i)
+		columns[m-1](i) = other.columns[m-1](i);
+}
+
+template<class T>
+HessMatrix<T>::~HessMatrix()
+{
+	 delete[] columns;
+}
+
+template<class T>
+HessMatrix<T> & HessMatrix<T>::operator=(const HessMatrix & other)
+{
+	 if(&other != this) copy(other);
+	 return *this;
+}
+
+template<class T> inline
+Size HessMatrix<T>::size1() const
+{
+	 return n;
+}
+
+template<class T> inline
+Size HessMatrix<T>::size2() const
+{
+	 return m;
+}
+
+template<class T> inline
+T & HessMatrix<T>::operator()(Index i, Index j)
+{
+	ASSERT( i < n );
+	ASSERT( j < m );
+
+	if(i < j+2)
+		return columns[j](i);
+
+	return zero;
+}
+
+template<class T> inline
+const T & HessMatrix<T>::operator()(Index i, Index j) const
+{
+	ASSERT( i < n );
+	ASSERT( j < m );
+
+	if(i < j+2)
+		return columns[j](i);
+
+	return zero;
+}
+
+template<class T>
+HessMatrix<T> & HessMatrix<T>::operator*=(const T & c)
+{
+	 for(Index j=0; j<m; ++j)
+		  columns[j] *= c;
+}
+
+template<class T>
+HessMatrix<T> & HessMatrix<T>::operator/=(const T & c)
+{
+	 for(Index j=0; j<m; ++j)
+		  columns[j] /= c;
+}
+
+template<class T>
+HessMatrix<T> & HessMatrix<T>::operator+=(const HessMatrix & other)
+{
+	 for(Index j=0; j<m; ++j)
+		  columns[j] += other.columns[j];
+}
+
+template<class T>
+HessMatrix<T> & HessMatrix<T>::operator-=(const HessMatrix & other)
+{
+	 for(Index j=0; j<m; ++j)
+		  columns[j] -= other.columns[j];
+}
+
+template<class T>
+void HessMatrix<T>::copy(const HessMatrix & other)
+{
+	 delete[] columns;
+
+	 n = other.n;
+	 m = other.m;
+	 columns = new Vector<T>[m];
+	 for(Index i=0; i<m; ++i)
+		  columns[i] = other.columns[i];
+}
 
 }}//::numlib::linalg
 
