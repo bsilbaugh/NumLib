efa2697f393c331d614f011a70fbe96b2a8162be
Benjamin Silbaugh <silbaugh@umd.edu>
2012-11-08 11:04:57 -0500

    Moved implementation of Krylov template into header

--- src/solvers/Krylov.h
+++ src/solvers/Krylov.h
@@ -4,6 +4,7 @@
 #ifndef KRYLOV_H
 #define KRYLOV_H
 
+#include "../base/nocopy.h"
 #include "../linalg/Vector.h"
 #include "../linalg/VectorExpressions.h"
 #include "../linalg/HessMatrix.h"
@@ -61,7 +62,61 @@ private:
 
 };
 
-#include "Krylov-inl.h"
+/*----------------------------------------------------------------------------*/
+/*                                                             IMPLEMENTATION */
+
+template<class T, class L, class K, class P>
+Krylov<T,L,K,P>::Krylov(Size n_, Size mmax_):
+  n(n_),m(0),mmax(mmax_),krylovSpace(n_,mmax_)
+{
+  ASSERT(mmax <= n);
+}
+
+template<class T, class L, class K, class P>
+T Krylov<T,L,K,P>::solve(const L & linO, VecType & x, 
+					   const VecType & b, const T & tol)
+{
+
+  // Initialize working data structure...
+
+  T zero(0);    // zero element of type T
+  VecType r(b); // Residual vector initialized to RHS vector, b
+  
+  // Compute residual due to initial guess x...
+
+  T xn = norm2(x);
+  if(xn > zero) r -= prod(linO, x);
+
+  T beta = norm2(r);
+  T rn = beta;
+
+  // Construct Krylov suspace...
+
+  krylovSpace.buildSpace(linO, r, tol); /* Arnoldi or Housholder */
+
+  m = krylovSpace.size();
+
+  // Get the projection of A (i.e. 'linO') on the Krylov subspace...
+
+  HessType hess(m, true);
+  krylovSpace.projA(hess);
+
+  // Compute correction vector...
+
+  VecType y(m);
+  VecType z(n);
+  rn = projectionScheme.solve(beta, hess, y); /* GMRES or Galerkin projection */
+  krylovSpace.map(y, z);
+
+  // Apply correction to intial guess...
+
+  x += z;
+
+  // Return residual norm...
+
+  return rn;
+	
+}
 
 }}//::numlib::solver
 
