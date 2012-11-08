99e73559d5ae82f850da18bc5343ec88589640fe
Benjamin Silbaugh <silbaugh@umd.edu>
2012-11-08 10:56:08 -0500

    Moved implementation of NewtonArnoldi template into header

--- src/solvers/NewtonArnoldi-inl.h
+++
@@ -1,7 +0,0 @@
-
-template<class T, class NL>
-NewtonArnoldi<T,NL>::NewtonArnoldi(Size n_, Size mmax_, Size lmax_, Real tol_):
-	 NewtonKrylov<T,NL,KrylovSpaceAO<T,GateauxFD<T,NL> >,GalerkinProjection<T> >(
-		  n_, mmax_, lmax_, tol_)
-{
-}
--- src/solvers/NewtonArnoldi.h
+++ src/solvers/NewtonArnoldi.h
@@ -16,22 +16,20 @@ namespace numlib{ namespace solver{
  *  Arnoldi's method (FOM) for the "inner" linear iteration.
  */
 template<class T, class NL>
-class NewtonArnoldi: public NewtonKrylov<T,
-										 NL,
-										 KrylovSpaceAO<T,GateauxFD<T,NL> >,
-										 GalerkinProjection<T> >
+class NewtonArnoldi: 
+    public NewtonKrylov< T, NL, KrylovSpaceAO< T, GateauxFD< T, NL > >, GalerkinProjection< T > >
 {
 public:
 
-	 NewtonArnoldi(Size n_, Size mmax_, Size lmax_, Real tol_);
+    NewtonArnoldi(Size n_, Size mmax_, Size lmax_, Real tol_):
+	    NewtonKrylov< T, NL, KrylovSpaceAO< T, GateauxFD< T, NL > >, GalerkinProjection<T> >
+        (n_, mmax_, lmax_, tol_) { }
 
 private:
 
 	 DISALLOW_COPY_AND_ASSIGN( NewtonArnoldi );
 
 };
-
-#include "NewtonArnoldi-inl.h"
 
 }}//::numlib::solver
 
